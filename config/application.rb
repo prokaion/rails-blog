d6c6536f7c71a6fdf40d7e7fd2809d9c158fc237
daniel <prokaion@alice.de>
2015-12-17 18:24:56 +0100

    changes made to development environments

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,7 @@ module Blog
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-    config.web_console.whitelisted_ips = '192.168.0.0/16'
+    config.active_record.raise_in_transactional_callbacks = true    
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,5 +42,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host }
+
+  config.web_console.whitelisted_ips = '192.168.0.0/16'
 end
 
