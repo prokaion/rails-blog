7e3f14e013941712bb451fdb61e8494e921e0f65
daniel <prokaion@alice.de>
2015-12-17 22:02:14 +0100

    host again4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,6 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'mighty-dawn-1718.herokuapp.com'
+  host = 'universum-25.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
 end
