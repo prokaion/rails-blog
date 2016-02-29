68972654da7fc55a6934ca24a02ea1985dbee37c
kaliyuga <kaliyuga@posteo.de>
2016-02-29 20:43:07 +0100

    capistrano2

--- db/migrate/20150918154619_create_comments.rb
+++ db/migrate/20150918154619_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.references :article, index: true, foreign_key: true
-      t.string :commenter,
+      t.string :commenter
       t.text :body
 
       t.timestamps null: false
